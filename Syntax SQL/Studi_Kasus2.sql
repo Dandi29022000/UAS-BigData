d64ed6c1edd87cdd93a9d02281bbb1c79e599ec2
Dandi29022000 <=>
2022-07-01 15:55:32 +0700

    Pembaruan Data

--- Syntax SQL/Studi_Kasus2.sql
+++ Syntax SQL/Studi_Kasus2.sql
@@ -1,10 +1,11 @@
 # STUDI KASUS 2
-# Melakukan pencarian data dengan menampilkan kota di India dan jumlah kasus kesembuhan paling banyak
+# Melakukan pencarian data dengan menampilkan state, recovered di negara India 
+# dan jumlah kasus kesembuhan paling banyak.
 
-SELECT 
-  state, 
+SELECT
+  state, recovered,
   COUNT(1) AS count
 FROM `covid-assistant.covid.india_covid` 
-  GROUP BY state
+  GROUP BY state, recovered
   ORDER BY count DESC
-  LIMIT 100
+LIMIT 100
